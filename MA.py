2216939a79124ac0b344c14ca4d8e07c3f9f7327
RomanSPbGASU <romanevsin@yandex.ru>
2020-04-10 15:18:44 +0300

    an algorithm have been implemented

--- MA.py
+++ MA.py
@@ -1,129 +1,86 @@
-# corr_matrix = [
-#     [1.000, -0.379, -0.366, -0.060, -0.552, -0.663],
-#     [-0.379, 1.000, -0.243, -0.126, 0.067, 0.140],
-#     [-0.366, -0.243, 1.000, -0.358, 0.208, 0.254],
-#     [-0.060, -0.126, -0.358, 1.000, -0.183, -0.068],
-#     [-0.552, 0.067, 0.208, -0.183, 1.000, 0.618],
-#     [-0.663, 0.140, 0.254, -0.068, 0.618, 1.000]
-# ]
-#
-# print(corr_matrix)
-# for i in range(0, 6):
-#     print(corr_matrix[i][i])
-#
-# M = 10
-# H = 100
-# U = 10
-# K = 50
-#
-# x_max = H^2*U/(2*K)
-#
-# pr_gr = 4*x_max
-#
-# h_x = 250
-#
-# h_z = H/4
-
-
-# import pandas as pd
-# import numpy as np
-# import matplotlib
-# import matplotlib.pyplot as plt
-# from scipy import stats
-# # matplotlib.style.use('ggplot')
-# # %matplotlib inline
-#
-# df = pd.read_csv('ArishaStatistics.csv', sep=';', decimal=',')
-# df.head()
-#
-# df.describe()
-# df.pop(1)
-import math
-import numpy as np
+from math import sqrt, pi, exp
 import matplotlib.pyplot as plt
+
+
+def tridiagonal_matrix_algorithm(a, b, c, f):
+    n = len(f)
+    alpha = [0] * n
+    beta = [0] * n
+    y = [0] * n
+
+    alpha[0] = b[0] / c[0]
+    beta[0] = -f[0] / c[0]
+
+    for i in range(1, n):
+        alpha[i] = b[i] / (c[i] - alpha[i - 1] * a[i])
+        beta[i] = (beta[i - 1] * a[i] - f[i]) / (c[i] - alpha[i - 1] * a[i])
+
+    y[-1] = beta[-1]
+
+    for i in range(n - 2, -1, -1):
+        y[i] = alpha[i] * y[i + 1] + beta[i]
+
+    return y
+
 
 M = 10  # мощность выброса (в граммах в секунду на метр) источника
 H = 40  # высота источника
 U = 4  # скорость ветра, направленного вдоль оси x
 K = 20  # коэффициент турбулентной диффузии
+dz = 1  # шаг по z [м]
+dx = 1  # шаг по x [м]
 
 x_max = H ** 2 * U / (2 * K)  # точка, в которой концентрация на нулевой высоте достигает максимума
 
+x_m = int(4 * x_max)  # граница по x
+z_n = max(4 * H, 100)  # граница по y
+# print(x_m, ' ', z_n)
 
-# print(x_max)
+# TODO: исправить приведение к int - выразить целочисленно
+n = int(z_n / dz)  # количество отрезков, на которые разбивается исследуемый интервал по z - размерность матрицы
+m = int(x_m / dx)  # количество отрезков по x - количество "слоёв" для расчёта
+i_s = int(H / dz)  # положение начальной концентрации рассеиваемого вещества (nH должно быть целым числом)
+c_i = M / (U * dz)  # начальное значение концентрации
+c_0 = [.0] * n
+c_0[i_s] = c_i  # вектор начальных концентраций
+# print("c_o", c_0)
+
+c = (2 * K + dz ** 2 / dx * U)  # коэффициент на главной диагонали
+A = [0] + [K] * (n - 1)  # диагональ под главной
+B = [-1] + [K] * (n - 1)  # диагональ над главной
+C = [-1] + [c] * (n - 2) + [-1]  # главная диагональ
+# print(A, '\n', B, '\n', C)
+
+c_prev = c_0
+results = [[0.] * n] * (m + 1)
+f = [0] * n
+p = -dz ** 2 / dx * U  # коэффициент функции
+
+results[0] = c_prev
+
+for xi in range(1, m + 1):
+    for i in range(1, n):
+        f[i] = p * c_prev[i]
+
+    c = tridiagonal_matrix_algorithm(A, B, C, f)
+    c_prev = c.copy()
+    results[xi] = c
+
+print(results)
 
 
 def ca(x):  # аналитическое решение
-    return M / (math.sqrt(U * math.pi * K * x)) * math.exp(-H ** 2 * U / (4 * K * x))
+    return M / (sqrt(U * pi * K * x)) * exp(-H ** 2 * U / (4 * K * x))
 
-
-pr_gr = int(4 * x_max)
-# print(pr_gr, type(pr_gr))
 
 dots = []
 
-for i in range(1, pr_gr):
+for i in range(1, x_m):
     dots.append(ca(i))
 
-
-h_x = 250
-h_z = 25
-
-CC = (2 * K + h_z ** 2 / h_x * U)
-
-hr = H / h_z
-
-el2 = M / (U * h_z)
-
-d_size = 16
-MA = np.eye(d_size, k=-1) * K + np.eye(d_size) * CC + np.eye(d_size, k=1) * K
-MA[0][0] = -1
-MA[0][1] = -1
-MA[d_size - 1][d_size - 1] = -1
-MA[d_size - 1][d_size - 2] = 0
-# print(MA)
-
-F = np.zeros(shape=[16], dtype=int)
-F[5] = M / U * h_z
-print(F)
-
-for i, elem in enumerate(F):
-    F[i] = -h_z ** 2 / h_x * U * elem
-
-A = np.diag(MA, k=-1)
-B = np.diag(MA, k=1)
-C = np.diag(MA)
-
-
-# print(A, '\n', B, '\n', C)
-
-
-def tridiagonal_matrix_algorithm(a, b, c, f):
-    alpha = [0]
-    beta = [0]
-    n = len(f)
-    x = [0] * n
-
-
-#    a, b, c, f = map(lambda k_list: map(float, k_list), (a, b, c, f))
-
-    for i in range(n - 1):
-        alpha.append(-b[i] / (a[i] * alpha[i] + c[i]))
-        beta.append((f[i] - a[i] * beta[i]) / (a[i] * alpha[i] + c[i]))
-
-    x[n - 1] = (f[n - 1] - a[n - 2] * beta[n - 1]) / (c[n - 1] + a[n - 2] * alpha[n - 1])
-
-    for i in reversed(range(n - 1)):
-        x[i] = alpha[i + 1] * x[i + 1] + beta[i + 1]
-
-    return x
-
-
-numeric = tridiagonal_matrix_algorithm(A, B, C, list(F))
-print(numeric)
-
-#plt.plot(dots, color='#f12')
-plt.plot(numeric, color='#b48')
+plt.plot(dots, color='#f12')
+transposed_results = list(zip(*results))
+plt.plot(transposed_results[0], color='#38a')
 plt.ylabel('c, г/м')
 plt.xlabel('x, м')
 plt.legend(['аналитическое решение', 'численное решение'])

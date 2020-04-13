8f40651efcbd886bccba5367d29fdf7bc60a9032
RomanSPbGASU <romanevsin@yandex.ru>
2020-04-13 05:04:14 +0300

    Refactored algorithms | Before tests

--- Genetic.py
+++ Genetic.py
@@ -58,85 +58,85 @@ def roulette(cum_sum, chance):
     return sorted([*cum_sum.copy(), chance]).index(chance)
 
 
-
-
-def selection(generation: Generation, method: SelectionMethod = SelectionMethod.FITNEST_HALF):
+def selection(generation: Generation, method: SelectionMethod = SelectionMethod.FITNEST_HALF) -> Generation:
     # для каждого из фитнесов в генерэйшене вычислим его нормализованное значение разделив его на сумму всех фитнесов
     generation.normalized_fitness = sorted(
         [fitness_value / sum(generation.fitness) for fitness_value in generation.fitness],
         reverse=True)
     generation.cumulative_sum = np.array(generation.normalized_fitness).cumsum()
+    half_count = len(generation.individuals) // 2
     if method == SelectionMethod.ROULETTE_WHEEL:
-        selected = []
-        for x in range(len(generation.individuals) // 2):
-            selected.append(roulette(generation.cumulative_sum, rnd()))
+        selected_indexes = []
+        for x in range(half_count):
+            selected_indexes.append(None)
             # пока все элементы в списке не будут уникальными
-            while len(set(selected)) != len(selected):
-                selected[x] = (roulette(generation.cumulative_sum, rnd()))
-                selected = Generation()
-                selected.individuals = [generation.individuals[int(selected[x])] for x in
-                                             range(len(generation.individuals) // 2)]
-                selected.fitness = [generation.fitness[int(selected[x])] for x in
-                                         range(len(generation.individuals) // 2)]
+            while True:
+                selected_indexes[x] = (roulette(generation.cumulative_sum, rnd()))
+                if len(set(selected_indexes)) != len(selected_indexes):
+                    break
+        selected = Generation()
+        selected.individuals, selected.fitness = (
+            generation.individuals[int(selected_indexes[x])],
+            generation.fitness[int(selected_indexes[x])] for x in range(half_count))
     elif method == SelectionMethod.FITNEST_HALF:
-        selected_individuals = [generation.individuals[-x - 1] for x in
-                                range(int(len(generation.individuals) // 2))]
-        selected_fitnesses = [generation.fitness[-x - 1] for x in
-                              range(int(len(generation.individuals) // 2))]
-        selected = {
-            Generation.INDIVIDUALS: selected_individuals,
-            Generation.FITNESS: selected_fitnesses}
+        selected = Generation()
+        for x in range(half_count):
+            selected.individuals.append(generation.individuals[-x - 1])
+            selected.fitness.append(generation.fitness[-x - 1])
     elif method == SelectionMethod.RANDOM:
-        selected_individuals = [generation.individuals[randint(1, len(generation.fitness))] for
-                                x in range(int(len(generation.individuals) // 2))]
-        selected_fitnesses = [generation.fitness[-x - 1] for x in
-                              range(int(len(generation.individuals) // 2))]
-        selected = {
-            Generation.INDIVIDUALS: selected_individuals,
-            Generation.FITNESS: selected_fitnesses}
+        selected = Generation()
+        for x in range(half_count):
+            selected.individuals.append(generation.individuals[randint(1, len(generation.fitness))])
+            selected.fitness.append(generation.fitness[-x - 1])
     else:
         raise ValueError
     return selected
 
 
-def pairing(elit, selected, method='Fittest'):
-    individuals = [elit['Individuals']] + selected['Individuals']
-    fitness = [elit['Fitness']] + selected['Fitness']
+def pairing(elit: Generation, selected: Generation, method: PairingMethod = PairingMethod.FITTEST):
+    generation = Generation()
+    generation.individuals = [*elit.individuals, *selected.individuals]
+    generation.fitness = [*elit.fitness, *selected.fitness]
+    half_count = len(generation.individuals) // 2
+
     parents = []
-    if method == 'Fittest':
-        parents = [[individuals[x], individuals[x + 1]]
-                   for x in range(len(individuals) // 2)]
-    elif method == 'Random':
-        for x in range(len(individuals) // 2):
+    if method == PairingMethod.FITTEST:
+        parents = [[generation.individuals[x],
+                    generation.individuals[x + 1]]
+                   for x in range(half_count)]
+
+    elif method == PairingMethod.RANDOM:
+        last_index = len(generation.individuals) - 1
+        for x in range(half_count):
             parents.append(
-                [individuals[randint(0, (len(individuals) - 1))],
-                 individuals[randint(0, (len(individuals) - 1))]])
+                [generation.individuals[randint(0, last_index)],
+                 generation.individuals[randint(0, last_index)]])
             while parents[x][0] == parents[x][1]:
-                parents[x][1] = individuals[
-                    randint(0, (len(individuals) - 1))]
-    elif method == 'Weighted Random':
-        normalized_fitness = sorted(
-            [fitness[x] / sum(fitness)
-             for x in range(len(individuals) // 2)], reverse=True)
-        cummulitive_sum = np.array(normalized_fitness).cumsum()
-        for x in range(len(individuals) // 2):
+                parents[x][1] = generation.individuals[
+                    randint(0, last_index)]
+    elif method == PairingMethod.WEIGHTED_RANDOM:
+        normalized_fitness = sorted([generation.fitness[x] / sum(generation.fitness)
+                                     for x in range(half_count)],
+                                    reverse=True)
+        cumulative_sum = np.array(normalized_fitness).cumsum()
+        for x in range(half_count):
             parents.append(
-                [individuals[roulette(cummulitive_sum, rnd())],
-                 individuals[roulette(cummulitive_sum, rnd())]])
+                [generation.individuals[roulette(cumulative_sum, rnd())],
+                 generation.individuals[roulette(cumulative_sum, rnd())]])
             while parents[x][0] == parents[x][1]:
-                parents[x][1] = individuals[
-                    roulette(cummulitive_sum, rnd())]
+                parents[x][1] = generation.individuals[
+                    roulette(cumulative_sum, rnd())]
     else:
         raise ValueError
     return parents
 
 
-def mating(parents, method='Single Point'):
-    if method == 'Single Point':
+def mating(parents, method: MatingMethod = MatingMethod.SINGLE_POINT):
+    if method == MatingMethod.SINGLE_POINT:
         pivot_point = randint(1, len(parents[0]))
         offsprings = [parents[0][0:pivot_point] + parents[1][pivot_point:], parents[1]
         [0:pivot_point] + parents[0][pivot_point:]]
-    elif method == 'Two Pionts':
+    elif method == MatingMethod.TWO_POINTS:
         pivot_point_1 = randint(1, len(parents[0] - 1))
         pivot_point_2 = randint(1, len(parents[0]))
         while pivot_point_2 < pivot_point_1:
@@ -151,31 +151,30 @@ def mating(parents, method='Single Point'):
     return offsprings
 
 
-def mutation(individual, upper_limit, lower_limit, muatation_rate=2,
-             method='Reset', standard_deviation=0.001):
+def mutation(individual, upper_limit, lower_limit, mutation_rate=2,
+             method: MutationMethod = MutationMethod.RESET, standard_deviation=0.001):
     gene = [randint(0, 7)]
-    for x in range(muatation_rate - 1):
+    for x in range(mutation_rate - 1):
         gene.append(randint(0, 7))
         while len(set(gene)) < len(gene):
             gene[x] = randint(0, 7)
     mutated_individual = individual.copy()
-    if method == 'Gauss':
-        for x in range(muatation_rate):
+    if method == MutationMethod.GAUSS:
+        for x in range(mutation_rate):
             mutated_individual[x] = \
                 round(individual[x] + gauss(0, standard_deviation), 1)
-    if method == 'Reset':
-        for x in range(muatation_rate):
-            mutated_individual[x] = round(rnd() * \
-                                          (upper_limit - lower_limit) + lower_limit, 1)
+    if method == MutationMethod.RESET:
+        for x in range(mutation_rate):
+            mutated_individual[x] = round(rnd() * (upper_limit - lower_limit) + lower_limit, 1)
     return mutated_individual
 
 
-def next_generation(gen, upper_limit, lower_limit):
-    elit = {}
-    next_gen = {}
-    elit['Individuals'] = gen['Individuals'].pop(-1)
-    elit['Fitness'] = gen['Fitness'].pop(-1)
-    selected = selection(gen)
+def next_generation(generation: Generation, upper_limit, lower_limit):
+    elit = Generation()
+    elit.individuals = generation.individuals.pop(-1)
+    elit.fitness = generation.fitness.pop(-1)
+
+    selected = selection(generation)
     parents = pairing(elit, selected)
     offsprings = [[[mating(parents[x])
                     for x in range(len(parents))]
@@ -185,25 +184,25 @@ def next_generation(gen, upper_limit, lower_limit):
                    for x in range(len(parents))]
     offsprings2 = [offsprings[x][1]
                    for x in range(len(parents))]
-    unmutated = selected['Individuals'] + offsprings1 + offsprings2
+
+    unmutated = selected.individuals + offsprings1 + offsprings2
+
     mutated = [mutation(unmutated[x], upper_limit, lower_limit)
-               for x in range(len(gen['Individuals']))]
-    unsorted_individuals = mutated + [elit['Individuals']]
-    unsorted_next_gen = \
-        [fitness_calculation(mutated[x])
-         for x in range(len(mutated))]
+               for x in range(len(generation.individuals))]
+    unsorted_individuals = mutated + [elit.individuals]
+    unsorted_next_gen = [fitness_calculation(mutated[x]) for x in range(len(mutated))]
     unsorted_fitness = [unsorted_next_gen[x]
-                        for x in range(len(gen['Fitness']))] + [elit['Fitness']]
-    sorted_next_gen = \
-        sorted([[unsorted_individuals[x], unsorted_fitness[x]]
-                for x in range(len(unsorted_individuals))],
-               key=lambda x: x[1])
-    next_gen['Individuals'] = [sorted_next_gen[x][0]
-                               for x in range(len(sorted_next_gen))]
-    next_gen['Fitness'] = [sorted_next_gen[x][1]
-                           for x in range(len(sorted_next_gen))]
-    gen['Individuals'].append(elit['Individuals'])
-    gen['Fitness'].append(elit['Fitness'])
+                        for x in range(len(generation.fitness))] + [elit.fitness]
+    sorted_next_gen = sorted([[unsorted_individuals[x], unsorted_fitness[x]]
+                              for x in range(len(unsorted_individuals))],
+                             key=lambda x: x[1])
+
+    next_gen = Generation()
+    for x in range(len(sorted_next_gen)):
+        next_gen.individuals.append(sorted_next_gen[x][0])
+        next_gen.fitness.append(sorted_next_gen[x][1])
+    generation.individuals.append(elit.individuals)
+    generation.fitness.append(elit.fitness)
     return next_gen
 
 
@@ -220,11 +219,7 @@ def fitness_similarity_check(max_fitness, number_of_similarity):
     return result
 
 
-# Generations and fitness values will be written to this file
-Result_file = 'GA_Results.txt'  # Creating the First Generation
-
-
-def first_generation(pop):
+def first_generation(pop) -> Generation:
     fitness = [fitness_calculation(pop[x])
                for x in range(len(pop))]
     sorted_fitness = sorted([[pop[x], fitness[x]]
@@ -233,15 +228,20 @@ def first_generation(pop):
                   for x in range(len(sorted_fitness))]
     fitness = [sorted_fitness[x][1]
                for x in range(len(sorted_fitness))]
-    return {'Individuals': population, 'Fitness': sorted(fitness)}
+    generation = Generation()
+    generation.individuals = population
+    generation.fitness = sorted(fitness)
+    return generation
 
+
+# Generations and fitness values will be written to this file
+Result_file = 'GA_Results.txt'  # Creating the First Generation
 
 pop = population(20, 8, 1, 0)
 
 gen = [first_generation(pop)]
-fitness_avg = np.array([sum(gen[0]['Fitness']) /
-                        len(gen[0]['Fitness'])])
-fitness_max = np.array([max(gen[0]['Fitness'])])
+fitness_avg = np.array([sum(gen[0].fitness) / len(gen[0].fitness)])
+fitness_max = np.array([max(gen[0].fitness)])
 res = open(Result_file, 'a')
 res.write('\n' + str(gen) + '\n')
 res.close()
@@ -255,8 +255,8 @@ while not finish:
         break
     gen.append(next_generation(gen[-1], 1, 0))
     fitness_avg = np.append(fitness_avg, sum(
-        gen[-1]['Fitness']) / len(gen[-1]['Fitness']))
-    fitness_max = np.append(fitness_max, max(gen[-1]['Fitness']))
+        gen[-1].fitness) / len(gen[-1].fitness))
+    fitness_max = np.append(fitness_max, max(gen[-1].fitness))
     res = open(Result_file, 'a')
     res.write('\n' + str(gen[-1]) + '\n')
     res.close()
